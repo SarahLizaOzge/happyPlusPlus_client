f26020f11d51ce3511d7c3e72c7eee5abc4954c9
Sarah Bixler <bixler.s.e@gmail.com>
2018-01-02 13:45:19 -0800

    reset base in routes

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+if (window.location.pathname !== '/') page.base('/happyPlusPlus_client');
+
 page('/', () => app.pageView.initIndexPage());
 page('/first', () => app.pageView.firstPage());
 page('/second', () => app.pageView.secondPage());
